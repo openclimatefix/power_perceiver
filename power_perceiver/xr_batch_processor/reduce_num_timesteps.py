9f63f1c0f058558658bacb1a29070f3a837d918c
Jack Kelly <jack@openclimatefix.org>
2022-04-07 20:35:26 +0100

    ReduceNumTimesteps should select random timesteps. #44

--- power_perceiver/xr_batch_processor/reduce_num_timesteps.py
+++ power_perceiver/xr_batch_processor/reduce_num_timesteps.py
@@ -1,16 +1,35 @@
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Iterable, Union
+
+import numpy as np
 
 from power_perceiver.data_loader.data_loader import XarrayBatch
 
 
 @dataclass
 class ReduceNumTimesteps:
-    """Reduce the number of timesteps per example to `requested_timesteps`."""
+    """Reduce the number of timesteps per example to `requested_timesteps`.
 
-    requested_timesteps: Iterable[int]
+    If `requested_timesteps` is an int then randomly pick different timesteps for each batch.
+
+    If `requested_timesteps` is an array of ints then use that.
+    """
+
+    requested_timesteps: Union[int, Iterable[int]]
+    num_timesteps_available: int = 31
+
+    def __post_init__(self):
+        self.rng = np.random.default_rng()
 
     def __call__(self, xr_batch: XarrayBatch) -> XarrayBatch:
+        if isinstance(self.requested_timesteps, int):
+            requested_timesteps = self.rng.integers(
+                low=0, high=self.num_timesteps_available, size=self.requested_timesteps
+            )
+            print(requested_timesteps)
+        else:
+            requested_timesteps = self.requested_timesteps
+
         for name, xr_dataset in xr_batch.items():
-            xr_batch[name] = xr_dataset.isel(time=self.requested_timesteps)
+            xr_batch[name] = xr_dataset.isel(time=requested_timesteps)
         return xr_batch
