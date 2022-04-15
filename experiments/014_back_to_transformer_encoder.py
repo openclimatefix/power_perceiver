86b387b1588dc6bda02bed1c4d05429848c06f7d
Jack Kelly <jack@openclimatefix.org>
2022-04-15 16:43:00 +0100

    GPU

--- experiments/014_back_to_transformer_encoder.py
+++ experiments/014_back_to_transformer_encoder.py
@@ -229,7 +229,7 @@ wandb_logger = WandbLogger(
 wandb_logger.watch(model, log="all")
 
 trainer = pl.Trainer(
-    gpus=[4],
+    gpus=[3],
     max_epochs=70,
     logger=wandb_logger,
     callbacks=[
