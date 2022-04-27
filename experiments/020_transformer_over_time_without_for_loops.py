fe0f6d9115fcd97a16ad1005fd1232e6c9af7685
Jack Kelly <jack@openclimatefix.org>
2022-04-27 20:46:26 +0100

    set GPU

--- experiments/020_transformer_over_time_without_for_loops.py
+++ experiments/020_transformer_over_time_without_for_loops.py
@@ -393,7 +393,7 @@ wandb_logger = WandbLogger(
 # wandb_logger.watch(model, log="all")
 
 trainer = pl.Trainer(
-    gpus=[4],
+    gpus=[5],
     max_epochs=70,
     logger=wandb_logger,
     callbacks=[
