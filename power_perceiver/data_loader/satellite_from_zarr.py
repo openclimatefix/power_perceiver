d7c29a4b04de1452a552ad746c02e3409b794760
Jack Kelly <jack@openclimatefix.org>
2022-05-05 18:05:38 +0100

    bugfix

--- power_perceiver/data_loader/satellite_from_zarr.py
+++ power_perceiver/data_loader/satellite_from_zarr.py
@@ -195,8 +195,11 @@ class SatelliteDataset(torch.utils.data.IterableDataset):
         segment_idx = self.rng.integers(low=0, high=len(contiguous_segments))
         segment = contiguous_segments[segment_idx]
 
-        # Pick a random start time within the segment
-        max_legal_start_idx = segment[-1] - self.n_timesteps_per_example
+        # Pick a random start time within the segment.
+        # The +1 is necessary because segment[-1] gives the *index* which might be
+        # as low as self.n_timesteps_per_example - 1.
+        max_legal_start_idx = segment[-1] - self.n_timesteps_per_example + 1
+        assert max_legal_start_idx > 0, f"{max_legal_start_idx=}"
         start_idx = self.rng.integers(low=segment[0], high=max_legal_start_idx)
         end_idx = start_idx + self.n_timesteps_per_example
 
