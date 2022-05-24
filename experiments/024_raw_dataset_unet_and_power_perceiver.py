dd7e0358923221a17a312b4e55d32c434044ebe2
Jack Kelly <jack@openclimatefix.org>
2022-05-24 19:02:23 +0100

    bugfix

--- experiments/024_raw_dataset_unet_and_power_perceiver.py
+++ experiments/024_raw_dataset_unet_and_power_perceiver.py
@@ -452,15 +452,18 @@ class FullModel(pl.LightningModule):
         hrvsatellite = hrvsatellite[..., top:bottom, left:right]
         assert hrvsatellite.shape[-2] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
         assert hrvsatellite.shape[-1] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
+
+        # Crop spatial coordinates:
         for batch_key in (
             BatchKey.hrvsatellite_y_osgb_fourier,
             BatchKey.hrvsatellite_x_osgb_fourier,
             BatchKey.hrvsatellite_surface_height,
         ):
-            x[batch_key] = x[batch_key][..., top:bottom, left:right]
-            assert x[batch_key].shape[-2] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
-            assert x[batch_key].shape[-1] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
+            x[batch_key] = x[batch_key][:, top:bottom, left:right, ...]
+            assert x[batch_key].shape[1] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
+            assert x[batch_key].shape[2] == SATELLITE_TRANSFORMER_IMAGE_SIZE_PIXELS
 
+        # Pass through the transformer!
         sat_trans_out = self.satellite_transformer(x=x, hrvsatellite=hrvsatellite)
         pv_attn_out = sat_trans_out["pv_attn_out"]  # Shape: (example time n_pv_systems d_model)
         gsp_attn_out = sat_trans_out["gsp_attn_out"]
