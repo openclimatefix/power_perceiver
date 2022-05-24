1a98f9b21aad6a1a05f87f09ae9b2deb228cd361
Jack Kelly <jack@openclimatefix.org>
2022-05-24 19:05:33 +0100

    ipdb

--- experiments/024_raw_dataset_unet_and_power_perceiver.py
+++ experiments/024_raw_dataset_unet_and_power_perceiver.py
@@ -506,6 +506,7 @@ class FullModel(pl.LightningModule):
         # Concatenate all the things we're going to feed into the "time transformer":
         time_attn_in = (pv_attn_out, gsp_attn_out, gsp_query)
         time_attn_in = torch.concat(time_attn_in, dim=1)
+        import ipdb; ipdb.set_trace()
         time_attn_out = self.time_transformer(time_attn_in)
 
         power_out = self.pv_output_module(time_attn_out)  # (example, total_num_elements, 1)
