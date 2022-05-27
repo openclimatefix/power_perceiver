451f3456c85efcae8bf7eed41bfa9334289e274b
Jack Kelly <jack@openclimatefix.org>
2022-05-27 11:44:48 +0100

    bugfix

--- experiments/024_raw_dataset_unet_and_power_perceiver.py
+++ experiments/024_raw_dataset_unet_and_power_perceiver.py
@@ -780,7 +780,9 @@ class FullModel(pl.LightningModule):
         self.log(f"{self.tag}/gsp_neg_log_prob", gsp_neg_log_prob_loss)
 
         # GSP power loss:
-        gsp_distribution = get_distribution(predicted_gsp_power.num_to_nan(0))
+        # When we're not masking `predicted_gsp_power`, we have to nan_to_num because examples
+        # without GSP will be NaN, and the `get_distribution` machinery doesn't like NaNs!
+        gsp_distribution = get_distribution(predicted_gsp_power.nan_to_num(0))
         gsp_mse_loss = F.mse_loss(gsp_distribution.mean[gsp_mask], actual_gsp_power[gsp_mask])
         self.log(f"{self.tag}/gsp_mse", gsp_mse_loss)
 
