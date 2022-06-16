2851dedaa646eb147bdebf442a3cb70375544929
Jack Kelly <jack@openclimatefix.org>
2022-06-16 13:29:22 +0100

    reduce batch size to 16

--- experiments/026_dont_train_unet.py
+++ experiments/026_dont_train_unet.py
@@ -203,7 +203,7 @@ def get_dataloader(
     )
 
     raw_dataset_kwargs = dict(
-        n_examples_per_batch=20,  # TODO: Increase to more like 32!
+        n_examples_per_batch=16,  # TODO: Increase to more like 32!
         n_batches_per_epoch=n_batches_per_epoch_per_worker,
         np_batch_processors=np_batch_processors,
         load_subset_every_epoch=load_subset_every_epoch,
