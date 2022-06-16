db105ea88abff92a067f981114e87921727716ae
Jack Kelly <jack@openclimatefix.org>
2022-06-16 20:24:21 +0100

    bugfix

--- experiments/028_remove_rnn.py
+++ experiments/028_remove_rnn.py
@@ -543,7 +543,7 @@ class SatelliteTransformer(nn.Module):
 # See https://discuss.pytorch.org/t/typeerror-unhashable-type-for-my-torch-nn-module/109424/6
 @dataclass(eq=False)
 class FullModel(pl.LightningModule):
-    d_model: int = D_MODEL
+    d_model: int = D_MODEL + N_HEADS  # + N_HEADS for historical PV appended to attn out.
     pv_system_id_embedding_dim: int = 16
     num_heads: int = N_HEADS
     dropout: float = 0.1
