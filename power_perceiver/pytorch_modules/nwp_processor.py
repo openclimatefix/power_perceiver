739fd0c813deb4d686b9d6247df699629d13a76a
Jack Kelly <jack@openclimatefix.org>
2022-06-23 14:01:37 +0100

    Try giving the NWP as multiple query elements. e.g. each variable is its own element (with a learnt channel ID) #163

--- power_perceiver/pytorch_modules/nwp_processor.py
+++ power_perceiver/pytorch_modules/nwp_processor.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import einops
 import torch
 from torch import nn
@@ -5,9 +7,14 @@ from torch import nn
 from power_perceiver.consts import BatchKey
 
 
+@dataclass
 class NWPProcessor(nn.Module):
+    n_channels: int  # Number of NWP channels.
+    channel_id_dim: int = 16
+
     def __init__(self):
         super().__init__()
+        self.learnable_channel_ids = nn.Parameter(torch.randn(self.n_channels, self.channel_id_dim))
 
     def forward(self, x: dict[BatchKey, torch.Tensor]) -> torch.Tensor:
         """Return a byte array ready for Perceiver.
@@ -21,11 +28,36 @@ class NWPProcessor(nn.Module):
             tensor of shape (example, time, feature).
         """
 
-        # Patch all positions and all channels into a single element:
+        # Patch all positions into a single element:
         # TODO: Use proper encodings for each NWP channel?
         nwp = x[BatchKey.nwp]  # (example, time, channel, y, x)
-        nwp = einops.rearrange(nwp, "example time channel y x -> example time (channel y x)")
+        nwp = einops.rearrange(nwp, "example time channel y x -> example time channel (y x)")
+
+        # Repeat time fourier over all channels
+        assert nwp.shape[2] == self.n_channels
+        time_fourier = x[BatchKey.nwp_target_time_utc_fourier]  # (example, time, n_features)
+        time_fourier = einops.repeat(
+            time_fourier,
+            "example time features -> example time channel features",
+            channel=self.n_channels,
+        )
+
+        # Repeat learnt embedding ID for each channel:
+        channel_ids = einops.repeat(
+            self.learnable_channel_ids,
+            "channel features -> example time channel features",
+            example=nwp.shape[0],
+            time=nwp.shape[1],
+        )
 
         # Concatenate time fourier on the final dimension:
-        time_fourier = x[BatchKey.nwp_target_time_utc_fourier]  # (example, time, n_features)
-        return torch.concat((nwp, time_fourier), dim=-1).float()
+        nwp_query = torch.concat((time_fourier, channel_ids, nwp), dim=-1).float()
+        # Shape: example time channel features
+
+        import ipdb
+
+        ipdb.set_trace()
+
+        return einops.rearrange(
+            nwp_query, "example time channel features -> example (time channel) features"
+        )
